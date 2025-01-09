8d9bf9dbf93a47f958bdc6f559b91b7ca2106e2b
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-01-09 15:55:48 -0300

    Learning about document loaders

---
+++ 07-loaders/loader-csv.py
@@ -0,0 +1,39 @@
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import CSVLoader
+
+model = ChatGroq(model='llama-3.3-70b-versatile')
+
+loader = CSVLoader(file_path='07-loaders/dados/base_conhecimento.csv')
+documents = loader.load() # Retorna uma lista das linhas do documento
+
+print('DOCUMENTS:\n', documents)
+
+
+prompt_base_conhecimento = PromptTemplate(
+    input_variables=['document_content', 'pergunta'],
+    template='''Use o seguinte conteúdo do documento para responder à pergunta. 
+    Responda apenas com base nas informações fornecidas.
+    Não utilize informações externas ao conteúdo do documento:
+    Conteúdo do Documento: {document_content}
+    Pergunta: {pergunta}
+    '''
+)
+
+chain = (
+    prompt_base_conhecimento
+    | model
+    | StrOutputParser()
+)
+
+response = chain.stream(
+    {
+        'document_content': '\n'.join(doc.page_content for doc in documents),
+        'pergunta': 'Qual o conteúdo do arquivo?'
+    }
+)
+
+print('\nRESPONSE:\n')
+for chuck in response:
+    print(chuck, end='')
---
+++ 07-loaders/loader-txt.py
@@ -0,0 +1,40 @@
+from langchain_groq import ChatGroq
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders import TextLoader
+
+model = ChatGroq(model='llama-3.3-70b-versatile')
+
+loader = TextLoader(file_path='07-loaders/dados/base_conhecimento.txt')
+documents = loader.load() # Retorna uma lista das paginas do documento
+
+print('DOCUMENTS:\n', documents)
+
+
+prompt_base_conhecimento = PromptTemplate(
+    input_variables=['document_content', 'pergunta'],
+    template='''Use o seguinte conteúdo do documento para responder à pergunta. 
+    Responda apenas com base nas informações fornecidas.
+    Não utilize informações externas ao conteúdo do documento:
+    Conteúdo do Documento: {document_content}
+    Pergunta: {pergunta}
+    '''
+)
+
+chain = (
+    prompt_base_conhecimento
+    | model
+    | StrOutputParser()
+)
+
+response = chain.stream(
+    {
+        'document_content': '\n'.join(doc.page_content for doc in documents),
+        'pergunta': 'Qual o conteúdo do arquivo?'
+    }
+)
+
+print('\nRESPONSE:\n')
+
+for chuck in response:
+    print(chuck, end='')
